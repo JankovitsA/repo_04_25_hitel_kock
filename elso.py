e5f0fa920bf8d667efd6e466b035564d7f25a64b
SzilardPalma <szilard.palma@gmail.com>
2023-04-25 10:58:53 +0200

    Added new functions

--- elso.py
+++ elso.py
@@ -1,11 +1,18 @@
-def barmi(a,x):
-    print(a*x)
+import numpy as np
+import matplotlib.pyplot as plt
 
+def generate_synthetic_data(x, coefficients, seed=4, noise_std=1):
+    np.random.seed(seed)
+    y = np.polyval(coefficients[::-1], x) + np.random.normal(0, noise_std, len(x))
 
-def jajjnekem(a):
-    return a
+def visualize_data(x,y):
+    plt.scatter(x,y)
+    plt.xlabel("Feature (x)")
+    plit.title("Synthetic Data")
+    plt.show()
 
-print(jajjnekem("jajjjajj"))
-
-def osszead(a,b):
-    return a+b
+def main():
+    coefficients = [1, 0.02, -0.002, 0.014]
+    x_values = np.linspace(-10,10,100)
+    x,y = generate_synthetic_data(x_values,coefficients)
+    visualize_data(x,y)
